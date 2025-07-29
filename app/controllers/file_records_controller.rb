c416d66463bb55ac9232809f8d055e301000c7a0
cloud-digiteer <cloud@digiteer.digital>
2025-07-29 11:17:50 +0800

    Update table store only file names

--- app/controllers/file_records_controller.rb
+++ app/controllers/file_records_controller.rb
@@ -1,9 +1,12 @@
 class FileRecordsController < ApplicationController
+
   def index
     puts params
     @query = params[:query]
-    @sort_order = params[:sort]
-    @file_records = FileRecord.with_attached_file.sorted_by_created_at(@sort_order)
+    @sort_order = params[:sort].presence || "desc"
+    @file_records = FileRecord.sorted_by_created_at(@sort_order)
+    @months = Date::MONTHNAMES.compact.each_with_index.map { |month, i| [month, i + 1] }
+
     respond_to do |format|
       format.html
       format.turbo_stream do 
@@ -17,11 +20,22 @@ class FileRecordsController < ApplicationController
   def create
     @file_record = FileRecord.new(file_record_params)
     @file_record.status = "for_signed"
-    
+
     if @file_record.save
-      redirect_to file_records_path, notice: "File uploaded successfully."
+      @file_records = FileRecord.sorted_by_created_at("desc")
+      respond_to do |format|
+        format.turbo_stream do
+          render turbo_stream: turbo_stream.replace(
+            "file_records",
+            partial: "file_records/table",
+            locals: { file_records: @file_records }
+          )
+        end
+        format.html { redirect_to file_records_path, notice: "File added successfully." }
+      end
     else
-      redirect_to file_records_path, alert: "Failed to upload file."
+      puts "================ERROR=================="
+      puts @file_record.errors.full_messages.join(", ")
     end
   end
 
@@ -32,7 +46,9 @@ class FileRecordsController < ApplicationController
   end
 
   private
+
   def file_record_params
-    params.require(:file_record).permit(:file)
+    params.require(:file_record).permit(:filename)
   end
+
 end
--- app/models/file_record.rb
+++ app/models/file_record.rb
@@ -1,8 +1,4 @@
 class FileRecord < ApplicationRecord
-  has_one_attached :file
-  validates :file, presence: true 
-  before_save :set_filename
-
   scope :sorted_by_created_at, ->(direction = "desc") {
     order(created_at: direction)
   }
